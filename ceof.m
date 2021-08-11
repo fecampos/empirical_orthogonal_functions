a72fd98485e15982bfd1d0f5dbf42051a80dc5f2
Fernando Campos <32453254+fecampos@users.noreply.github.com>
2021-08-11 13:35:56 -0500

    Update ceof.m

--- ceof.m
+++ ceof.m
@@ -1,4 +1,4 @@
-function [re_space pc camp cpha tamp tpha expvar] = ceof(data,N)
+function [camp cpha tamp tpha expvar] = ceof(data,N)
 
 % [time x y] & time > x & time > y
 
@@ -23,19 +23,17 @@ end
 
 tamp = power(pc.*conj(pc),0.5);
 
-tpha = atan2(imag(pc),real(pc));
+tpha = angle(pc);
 
 spatial_amp = power(e.*conj(e),0.5);
 
-spatial_pha = atan2(imag(e),real(e));
+spatial_pha = angle(e);
 
-camp = NaN(dx*dy,N); cpha = NaN(dx*dy,N); re_space = NaN(dx*dy,N);
+camp = NaN(dx*dy,N); cpha = NaN(dx*dy,N);
 
 camp(ind,:) = spatial_amp'; camp = reshape(camp,dx,dy,N);
 
 cpha(ind,:) = spatial_pha'; cpha = reshape(cpha,dx,dy,N);
-
-re_space(ind,:) = e'; re_space = reshape(re_space,dx,dy,N);
 
 L = diag(L).^2;
 
